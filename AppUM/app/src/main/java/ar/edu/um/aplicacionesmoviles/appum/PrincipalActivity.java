595ebb1fd9fd202eec67d5b3a3bb2578fba7d694
FerSO-PC\Fer <fer.ochoa.ar@gmail.com>
2016-11-23 22:35:18 -0300

    no message

--- AppUM/app/src/main/java/ar/edu/um/aplicacionesmoviles/appum/PrincipalActivity.java
+++ AppUM/app/src/main/java/ar/edu/um/aplicacionesmoviles/appum/PrincipalActivity.java
@@ -1,7 +1,9 @@
 package ar.edu.um.aplicacionesmoviles.appum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    public void goMap(View view)
+    {
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
     }
 }
